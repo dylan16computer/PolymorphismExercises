0eb4172cef959cf015e4e797257eb3258bfa4e02
Dylan Hodder <30002799@student.toiohomai.ac.nz>
2018-08-27 11:57:09 +1200

    Task1 complete.

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ITStu1 = new ITStudent(79, 56, 48);
+            Student ITStu2 = new ITStudent(86, 12, 63);
+            Student ITStu3 = new ITStudent(54, 0, 25);
+            Student ITStu4 = new ITStudent(23, 79, 99);
+            Student ITStu5 = new ITStudent(19, 74, 87);
+            Student BusStu1 = new BusinessStudent(50, 46, 35);
+            Student BusStu2 = new BusinessStudent(100, 78, 44);
+            Student BusStu3 = new BusinessStudent(79, 38, 36);
+            Student BusStu4 = new BusinessStudent(13, 100, 100);
+            Student BusStu5 = new BusinessStudent(100, 100, 100);
+            List<Student> list = new List<Student>
+            {
+                ITStu1, ITStu2, ITStu3, ITStu4, ITStu5, BusStu1, BusStu2, BusStu3, BusStu4, BusStu5
+            };
+            Console.WriteLine("--- IT Students' Sum of Grades ---\n");
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.WriteLine(list[i].SumOfGrades());
+            //}
+            //for (int i = 6; i < 11; i++)
+            //{
+            //    Console.WriteLine(list[i].HighestGrade());
+            //}
+            foreach(Student x in list)
+            {
+                if(x.GetType().ToString() == "Task1.ITStudent")
+                    Console.WriteLine("-- Sum of Grades ---");
+                    Console.WriteLine(x.SumOfGrades());
+                if (x.GetType().ToString() == "Task1.BusinessStudent")
+                    Console.WriteLine("--- Highest Grade ---");
+                    Console.WriteLine(x.HighestGrade());
+            }
+        }
+    }
+}
---
+++ Task1/Task1/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Student
+    {
+        public int Grade1 { get; set; }
+        public int Grade2 { get; set; }
+        public int Grade3 { get; set; }
+        public virtual int SumOfGrades()
+        {
+            return Grade1 + Grade2 + Grade3;
+        }
+        public virtual int HighestGrade()
+        {
+            if (Grade1 > Grade2 && Grade1 > Grade3)
+            {
+                return Grade1;
+            }
+            else if (Grade2 > Grade1 && Grade2 > Grade3)
+            {
+                return Grade2;
+            }
+            else
+            {
+                return Grade3;
+            }
+        }
+    }
+    class ITStudent:Student
+    {
+
+        public override int SumOfGrades()
+        {
+            return base.SumOfGrades() / 3;
+        }
+        public ITStudent(int _grade1, int _grade2, int _grade3) 
+        {
+            Grade1 = _grade1;
+            Grade2 = _grade2;
+            Grade3 = _grade3;
+        }
+    }
+    class BusinessStudent:Student
+    {
+        public override int HighestGrade()
+        {
+            if(Grade1 > Grade2 && Grade1 > Grade3)
+            {
+                return Grade1;
+            }
+            else if(Grade2 > Grade1 && Grade2 > Grade3)
+            {
+                return Grade2;
+            }
+            else
+            {
+                return Grade3;
+            }
+        }
+        public BusinessStudent(int _grade1, int _grade2, int _grade3)
+        {
+            Grade1 = _grade1;
+            Grade2 = _grade2;
+            Grade3 = _grade3;
+        }
+    }
+}
